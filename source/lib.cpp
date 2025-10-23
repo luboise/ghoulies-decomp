726892a66bf14b6cf9a6b8e1ae71b0f243db28f1
luboise <lucas2@live.com.au>
2025-10-24 06:37:22 +1100

    Added triangle

--- source/lib.cpp
+++ source/lib.cpp
@@ -54,16 +54,24 @@ GhouliesLib::GhouliesLib()
 
   SDL_ClaimWindowForGPUDevice(device_, window_);
 
+  /*
+
   // Get window surface
-  SDL_Surface* surface = SDL_GetWindowSurface(window_);
+  SDL_Surface* surface {SDL_GetWindowSurface(window_)};
+
+  if (surface == nullptr) {
+    std::cerr << "Unable to acquire window surface.";
+    return;
+  }
 
   SDL_FillSurfaceRect(
       surface, nullptr, SDL_MapSurfaceRGB(surface, 0xFF, 0xFF, 0xFF));
 
   SDL_UpdateWindowSurface(window_);
+  */
 
-  auto vs_bytes = ReadFile("source/shaders/pbr.vert").value();
-  auto fs_bytes = ReadFile("source/shaders/pbr.frag").value();
+  auto vs_bytes = ReadFile("source/shaders/pbr_vert.spv").value();
+  auto fs_bytes = ReadFile("source/shaders/pbr_frag.spv").value();
 
   const SDL_GPUShaderCreateInfo vs_create_info {
       .code_size = vs_bytes.size(),
@@ -74,8 +82,7 @@ GhouliesLib::GhouliesLib()
       .num_samplers = 0,
       .num_storage_textures = 0,
       .num_storage_buffers = 0,
-      .num_uniform_buffers = 0,
-      .props = 0};
+      .num_uniform_buffers = 0};
 
   const SDL_GPUShaderCreateInfo fs_create_info {
       .code_size = fs_bytes.size(),
@@ -86,8 +93,7 @@ GhouliesLib::GhouliesLib()
       .num_samplers = 0,
       .num_storage_textures = 0,
       .num_storage_buffers = 0,
-      .num_uniform_buffers = 0,
-      .props = 0};
+      .num_uniform_buffers = 0};
 
   pbr_vert_shader_ = SDL_CreateGPUShader(device_, &vs_create_info);
   if (pbr_vert_shader_ == nullptr) {
@@ -103,38 +109,85 @@ GhouliesLib::GhouliesLib()
     return;
   }
 
+  std::array attributes = {
+      SDL_GPUVertexAttribute {.location = 0,
+                              .buffer_slot = 0,
+                              .format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+                              .offset = 0},
+
+      SDL_GPUVertexAttribute {.location = 1,
+                              .buffer_slot = 0,
+                              .format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+                              .offset = 12},
+
+      SDL_GPUVertexAttribute {.location = 2,
+                              .buffer_slot = 0,
+                              .format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+                              .offset = 24},
+
+      SDL_GPUVertexAttribute {.location = 3,
+                              .buffer_slot = 0,
+                              .format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT2,
+                              .offset = 36},
+
+  };
+
+  std::array vb_descriptions = {SDL_GPUVertexBufferDescription {
+      .slot = 0,
+      .pitch = sizeof(PBRVertex),
+      .input_rate = SDL_GPU_VERTEXINPUTRATE_VERTEX}};
+
+  SDL_GPUVertexInputState input_state {
+      .vertex_buffer_descriptions = vb_descriptions.data(),
+      .num_vertex_buffers = 1,
+      .vertex_attributes = attributes.data(),
+      .num_vertex_attributes = 4,
+  };
+
+  std::array color_target_descriptions = {SDL_GPUColorTargetDescription {
+      .format = SDL_GPU_TEXTUREFORMAT_B8G8R8A8_UNORM,
+      .blend_state {.enable_blend = false}}};
+
   SDL_GPUGraphicsPipelineCreateInfo pipeline_create_info {
       .vertex_shader = pbr_vert_shader_,
       .fragment_shader = pbr_frag_shader_,
 
+      .vertex_input_state = input_state,
+
       .rasterizer_state = SDL_GPURasterizerState {},
 
-      .multisample_state = SDL_GPUMultisampleState {},
+      .multisample_state =
+          SDL_GPUMultisampleState {.sample_count = SDL_GPU_SAMPLECOUNT_1},
 
-      .depth_stencil_state = SDL_GPUDepthStencilState {},
+      .depth_stencil_state =
+          SDL_GPUDepthStencilState {
+              .enable_depth_test = false,
+              .enable_stencil_test = false,
+          },
 
-      .target_info = SDL_GPUGraphicsPipelineTargetInfo {},
-  };
+      .target_info = SDL_GPUGraphicsPipelineTargetInfo {
+          .color_target_descriptions = color_target_descriptions.data(),
+          .num_color_targets = color_target_descriptions.size(),
+          .has_depth_stencil_target = false,
+      }};
 
-  this->pbr_pipeline_ =
-      SDL_CreateGPUGraphicsPipeline(device_, &pipeline_create_info);
-
-  if (this->pbr_pipeline_ == nullptr) {
+  pbr_pipeline_ = SDL_CreateGPUGraphicsPipeline(device_, &pipeline_create_info);
+  if (pbr_pipeline_ == nullptr) {
     std::cerr << std::format(
         "Failed to create pbr graphics pipeline! SDL_Error: {}\n",
         SDL_GetError());
     return;
   }
 
-  this->initialised_ = true;
+  initialised_ = true;
 }
 
 GhouliesLib::~GhouliesLib()
 {
-  SDL_ReleaseGPUShader(this->device_, this->pbr_vert_shader_);
-  SDL_ReleaseGPUShader(this->device_, this->pbr_frag_shader_);
+  SDL_ReleaseGPUShader(device_, pbr_vert_shader_);
+  SDL_ReleaseGPUShader(device_, pbr_frag_shader_);
 
-  SDL_ReleaseGPUGraphicsPipeline(this->device_, this->pbr_pipeline_);
+  SDL_ReleaseGPUGraphicsPipeline(device_, pbr_pipeline_);
 
   SDL_DestroyGPUDevice(device_);
   SDL_DestroyWindow(window_);
@@ -166,8 +219,29 @@ void GhouliesLib::DrawRectangle()
   std::array indices {Uint16 {0}, Uint16 {1}, Uint16 {2}};
   const Uint32 indices_size = sizeof(Uint16) * indices.size();
 
+  // TODO: Check command buffer is not null
   auto* command_buffer = SDL_AcquireGPUCommandBuffer(this->device_);
 
+  SDL_GPUTexture* swapchain_texture {nullptr};
+
+  Uint32 swapchain_width {};
+  Uint32 swapchain_height {};
+
+  SDL_Log("Acquiring swapchain.");
+  if (!SDL_WaitAndAcquireGPUSwapchainTexture(command_buffer,
+                                             window_,
+                                             &swapchain_texture,
+                                             &swapchain_width,
+                                             &swapchain_height)
+
+  )
+  {
+    SDL_CancelGPUCommandBuffer(command_buffer);
+    SDL_Log("Failed to acquire swapchain texture.");
+    return;
+  }
+
+  SDL_Log("Creating buffers.");
   // Create buffers
   {
     buffer_create_info.usage = SDL_GPU_BUFFERUSAGE_VERTEX;
@@ -185,8 +259,8 @@ void GhouliesLib::DrawRectangle()
     throw std::runtime_error("Bad ptr.");
   }
 
+  SDL_Log("Writing buffers.");
   {  // Transfer data
-
     auto* copy_pass {SDL_BeginGPUCopyPass(command_buffer)};
 
     if (copy_pass == nullptr) {
@@ -198,7 +272,7 @@ void GhouliesLib::DrawRectangle()
 
     SDL_GPUTransferBufferCreateInfo tb_info {
         .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
-        .size = std::max(vertices_size, indices_size)};
+        .size = vertices_size + indices_size};
     auto* transfer_buffer {
         SDL_CreateGPUTransferBuffer(this->device_, &tb_info)};
 
@@ -212,56 +286,66 @@ void GhouliesLib::DrawRectangle()
     transfer_region.size = vertices_size;
     SDL_UploadToGPUBuffer(copy_pass, &tb_location, &transfer_region, false);
 
-    std::memcpy(ptr, indices.data(), indices_size);
+    std::memcpy(((char*)ptr) + vertices_size, indices.data(), indices_size);
     transfer_region.buffer = index_buffer;
     transfer_region.size = indices_size;
+    tb_location.offset = vertices_size;
     SDL_UploadToGPUBuffer(copy_pass, &tb_location, &transfer_region, false);
 
     SDL_UnmapGPUTransferBuffer(this->device_, transfer_buffer);
     SDL_ReleaseGPUTransferBuffer(this->device_, transfer_buffer);
   }
 
-  SDL_GPUColorTargetInfo color_target_info {};
-  SDL_GPUDepthStencilTargetInfo depth_stencil_target_info {};
+  SDL_Log("Beginning render pass.");
+  SDL_GPUColorTargetInfo color_target_info {.texture = swapchain_texture,
+                                            .clear_color = {0.2, 0.2, 0.2, 1},
+                                            .load_op = SDL_GPU_LOADOP_CLEAR,
+                                            .store_op = SDL_GPU_STOREOP_STORE};
 
   // Create render pass and do commands
-  SDL_GPURenderPass* render_pass = SDL_BeginGPURenderPass(
-      command_buffer, &color_target_info, 1, &depth_stencil_target_info);
+  SDL_GPURenderPass* render_pass =
+      SDL_BeginGPURenderPass(command_buffer, &color_target_info, 1, nullptr);
 
   if (render_pass == nullptr) {
     throw std::runtime_error("Bad render pass.");
   }
 
+  SDL_Log("Binding Graphics Pipeline.");
   SDL_BindGPUGraphicsPipeline(render_pass, this->pbr_pipeline_);
 
   SDL_GPUViewport viewport {.x = 0,
                             .y = 0,
-                            .w = kWindowWidth,
-                            .h = kWindowHeight,
+                            .w = static_cast<float>(swapchain_width),
+                            .h = static_cast<float>(swapchain_height),
                             .min_depth = 0,
                             .max_depth = 1};
 
+  SDL_Log("Setting viewport.");
   SDL_SetGPUViewport(render_pass, &viewport);
 
   void* buffer = nullptr;
 
+  SDL_Log("Binding vertex buffers.");
   std::array<SDL_GPUBufferBinding, 1> vb_bindings {
       SDL_GPUBufferBinding {.buffer = vertex_buffer, .offset = 0}};
   SDL_BindGPUVertexBuffers(render_pass, 0, vb_bindings.data(), 1);
 
   SDL_GPUBufferBinding ib_binding {.buffer = index_buffer, .offset = 0};
 
+  SDL_Log("Binding index buffer.");
   SDL_BindGPUIndexBuffer(
       render_pass, &ib_binding, SDL_GPU_INDEXELEMENTSIZE_16BIT);
 
   // SDL_BindGPUVertexSamplers()
 
-  SDL_DrawGPUPrimitives(render_pass, vertices.size(), 0, 0, 0);
+  SDL_Log("Drawing primitives.");
+  SDL_DrawGPUIndexedPrimitives(render_pass, indices.size(), 1, 0, 0, 0);
 
+  SDL_Log("Ending render pass.");
   SDL_EndGPURenderPass(render_pass);
 
   SDL_SubmitGPUCommandBuffer(command_buffer);
 
-  SDL_ReleaseGPUBuffer(this->device_, vertex_buffer);
-  SDL_ReleaseGPUBuffer(this->device_, index_buffer);
+  SDL_ReleaseGPUBuffer(device_, vertex_buffer);
+  SDL_ReleaseGPUBuffer(device_, index_buffer);
 };
